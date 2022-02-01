51d1c8e4c8f7b0566ddc90585fd188879917a7d6
jguerreiro <joel.borghi@gmail.com>
2022-02-01 16:31:31 -0300

    update - centralizar likes com figura

--- js/thumbs.js
+++ js/thumbs.js
@@ -20,22 +20,38 @@ $(document).ready(function(){
     btn_thumb_recepcao.on('click', clickRecepcao);
 
     function clickPousada(){
+        if (btn_thumb_pousada.hasClass('disabled')){
+            return;
+        }
         var cont1 = parseInt(contador1.text());
         contador1.text(cont1 + 1);
+        btn_thumb_pousada.addClass('disabled');
     };
 
     function clickQuartos(){
+        if (btn_thumb_quartos.hasClass('disabled')){
+            return;
+        }
         var cont2 = parseInt(contador2.text());
         contador2.text(cont2 + 1);
+        btn_thumb_quartos.addClass('disabled');
     };
 
     function clickAlimentacao(){
+        if (btn_thumb_alimentacao.hasClass('disabled')){
+            return;
+        }
         var cont3 = parseInt(contador3.text());
         contador3.text(cont3 + 1);
+        btn_thumb_alimentacao.addClass('disabled');
     };
 
     function clickRecepcao(){
+        if (btn_thumb_recepcao.hasClass('disabled')){
+            return;
+        }
         var cont4 = parseInt(contador4.text());
         contador4.text(cont4 + 1);
+        btn_thumb_recepcao.addClass('disabled');
     };
 });
